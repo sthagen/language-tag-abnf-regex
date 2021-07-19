436c998b5fe4b8528a2affd7e95fe4776cabea40
Stefan Hagen <stefan@hagen.link>
2021-07-19 09:17:11 +0200

    Parameterize tests to support all in one feedback
    When not setting up parameters the developer only receives incremental feedback per data point in case of multiple failures.

--- tests/test_regex.py
+++ tests/test_regex.py
@@ -13,41 +13,41 @@ with open(PATTERN_SOURCE_PATH, 'rt', encoding=ENCODING) as pattern_source:
 TAG_SOURCE_ROOT = pathlib.Path('tests', 'abnfgen', 'language-tag-n-100-s-42')
 TAG_SOURCE_NAME_TEMPLATE = 'language-tag-{}.txt'
 
+WELL_KNOWN_TAGS = (
+    'de',
+    'de-CH',
+    'de-DE-1901',
+    'es-419',
+    'sl-IT-nedis',
+    'en-US-boont',
+    'mn-Cyrl-MN',
+    'x-fr-CH',
+    'en-GB-boont-r-extended-sequence-x-private',
+    'sr-Cyrl',
+    'sr-Latn',
+    'hy-Latn-IT-arevela',
+    'zh-TW',
+)
 
 def match(pattern: str, text: str):
     """Proxy method to evaluate the pattern match on text."""
     return re.match(pattern, text)
 
 
+@pytest.mark.parametrize('tag', WELL_KNOWN_TAGS)
 def test_ok_match_on_well_known_tags():
-    well_known_tags = (
-        'de',
-        'de-CH',
-        'de-DE-1901',
-        'es-419',
-        'sl-IT-nedis',
-        'en-US-boont',
-        'mn-Cyrl-MN',
-        'x-fr-CH',
-        'en-GB-boont-r-extended-sequence-x-private',
-        'sr-Cyrl',
-        'sr-Latn',
-        'hy-Latn-IT-arevela',
-        'zh-TW',
-    )
-    for tag in well_known_tags:
-        assert match(PATTERN, tag)
+    assert match(PATTERN, tag)
 
 
 def test_nok_do_not_match_empty_tag():
     assert not match(PATTERN, '')
 
 
-def test_ok_match_on_abnfgen_cases():
-    for number in range(1, 101):
-        tag_source_path = pathlib.Path(TAG_SOURCE_ROOT, TAG_SOURCE_NAME_TEMPLATE.format(number))
-        with open(tag_source_path, 'rt', encoding=ENCODING) as tag_source:
-            tag = tag_source.read()
-        actual = match(PATTERN, tag)
-        if actual is None:
-            raise RuntimeError("Failed to match tag (%s) from source (%s)" % (tag, tag_source_path))
+@pytest.mark.parametrize('number', tuple(range(1, 101)))
+def test_ok_match_on_abnfgen_cases(number):
+    tag_source_path = pathlib.Path(TAG_SOURCE_ROOT, TAG_SOURCE_NAME_TEMPLATE.format(number))
+    with open(tag_source_path, 'rt', encoding=ENCODING) as tag_source:
+        tag = tag_source.read()
+    actual = match(PATTERN, tag)
+    if actual is None:
+        raise RuntimeError("Failed to match tag (%s) from source (%s)" % (tag, tag_source_path))
